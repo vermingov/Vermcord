963e668b9dff15d29e9726efe3b0fb35f309ac6e
Verm <vermin.gov@proton.me>
2025-11-02 22:10:19 +0100

    HTN

--- src/components/settings/tabs/updater/index.tsx
+++ src/components/settings/tabs/updater/index.tsx
@@ -14,15 +14,26 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
 import { useSettings } from "@api/Settings";
 import { Divider } from "@components/Divider";
 import { FormSwitch } from "@components/FormSwitch";
 import { Link } from "@components/Link";
-import { handleSettingsTabError, SettingsTab, wrapTab } from "@components/settings/tabs/BaseTab";
+import {
+    handleSettingsTabError,
+    SettingsTab,
+    wrapTab,
+} from "@components/settings/tabs/BaseTab";
 import { Margins } from "@utils/margins";
-import { ModalCloseButton, ModalContent, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
+import {
+    ModalCloseButton,
+    ModalContent,
+    ModalProps,
+    ModalRoot,
+    ModalSize,
+    openModal,
+} from "@utils/modal";
 import { useAwaiter } from "@utils/react";
 import { getRepo, isNewer, UpdateLogger } from "@utils/updater";
 import { Forms, React } from "@webpack/common";
@@ -36,46 +47,44 @@ function Updater() {
 
     const [repo, err, repoPending] = useAwaiter(getRepo, {
         fallbackValue: "Loading...",
-        onError: e => UpdateLogger.error("Failed to retrieve repo", err)
+        onError: (e) => UpdateLogger.error("Failed to retrieve repo", err),
     });
 
     const commonProps: CommonProps = {
         repo,
-        repoPending
+        repoPending,
     };
 
     return (
-        <SettingsTab title="Vencord Updater">
+        <SettingsTab title="Vermcord Updater">
             <Forms.FormTitle tag="h5">Updater Settings</Forms.FormTitle>
 
             <FormSwitch
                 title="Automatically update"
                 description="Automatically update Vencord without confirmation prompt"
                 value={settings.autoUpdate}
-                onChange={(v: boolean) => settings.autoUpdate = v}
+                onChange={(v: boolean) => (settings.autoUpdate = v)}
             />
             <FormSwitch
                 title="Get notified when an automatic update completes"
                 description="Show a notification when Vencord automatically updates"
                 value={settings.autoUpdateNotification}
-                onChange={(v: boolean) => settings.autoUpdateNotification = v}
+                onChange={(v: boolean) => (settings.autoUpdateNotification = v)}
                 disabled={!settings.autoUpdate}
             />
 
             <Forms.FormTitle tag="h5">Repo</Forms.FormTitle>
 
             <Forms.FormText>
-                {repoPending
-                    ? repo
-                    : err
-                        ? "Failed to retrieve - check console"
-                        : (
-                            <Link href={repo}>
-                                {repo.split("/").slice(-2).join("/")}
-                            </Link>
-                        )
-                }
-                {" "}
+                {repoPending ? (
+                    repo
+                ) : err ? (
+                    "Failed to retrieve - check console"
+                ) : (
+                    <Link href={repo}>
+                        {repo.split("/").slice(-2).join("/")}
+                    </Link>
+                )}{" "}
                 (<HashLink hash={gitHash} repo={repo} disabled={repoPending} />)
             </Forms.FormText>
 
@@ -83,33 +92,40 @@ function Updater() {
 
             <Forms.FormTitle tag="h5">Updates</Forms.FormTitle>
 
-            {isNewer
-                ? <Newer {...commonProps} />
-                : <Updatable {...commonProps} />
-            }
+            {isNewer ? (
+                <Newer {...commonProps} />
+            ) : (
+                <Updatable {...commonProps} />
+            )}
         </SettingsTab>
     );
 }
 
-export default IS_UPDATER_DISABLED
-    ? null
-    : wrapTab(Updater, "Updater");
+export default IS_UPDATER_DISABLED ? null : wrapTab(Updater, "Updater");
 
 export const openUpdaterModal = IS_UPDATER_DISABLED
     ? null
     : function () {
-        const UpdaterTab = wrapTab(Updater, "Updater");
+          const UpdaterTab = wrapTab(Updater, "Updater");
 
-        try {
-            openModal(wrapTab((modalProps: ModalProps) => (
-                <ModalRoot {...modalProps} size={ModalSize.MEDIUM}>
-                    <ModalContent className="vc-updater-modal">
-                        <ModalCloseButton onClick={modalProps.onClose} className="vc-updater-modal-close-button" />
-                        <UpdaterTab />
-                    </ModalContent>
-                </ModalRoot>
-            ), "UpdaterModal"));
-        } catch {
-            handleSettingsTabError();
-        }
-    };
+          try {
+              openModal(
+                  wrapTab(
+                      (modalProps: ModalProps) => (
+                          <ModalRoot {...modalProps} size={ModalSize.MEDIUM}>
+                              <ModalContent className="vc-updater-modal">
+                                  <ModalCloseButton
+                                      onClick={modalProps.onClose}
+                                      className="vc-updater-modal-close-button"
+                                  />
+                                  <UpdaterTab />
+                              </ModalContent>
+                          </ModalRoot>
+                      ),
+                      "UpdaterModal",
+                  ),
+              );
+          } catch {
+              handleSettingsTabError();
+          }
+      };
