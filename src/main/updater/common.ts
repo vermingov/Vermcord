c21596f0baa26fbd0cf51f4155d712714e9bc1ff
Verm <vermin.gov@proton.me>
2025-11-02 22:05:39 +0100

    HTN

--- src/main/updater/common.ts
+++ src/main/updater/common.ts
@@ -1,21 +1,3 @@
-/*
- * Vencord, a modification for Discord's desktop app
- * Copyright (c) 2022 Vendicated and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 export const VENCORD_FILES = [
     IS_DISCORD_DESKTOP ? "patcher.js" : "vencordDesktopMain.js",
     IS_DISCORD_DESKTOP ? "preload.js" : "vencordDesktopPreload.js",
@@ -28,18 +10,21 @@ export function serializeErrors(func: (...args: any[]) => any) {
         try {
             return {
                 ok: true,
-                value: await func(...arguments)
+                value: await func(...arguments),
             };
         } catch (e: any) {
             return {
                 ok: false,
-                error: e instanceof Error ? {
-                    // prototypes get lost, so turn error into plain object
-                    ...e,
-                    message: e.message,
-                    name: e.name,
-                    stack: e.stack
-                } : e
+                error:
+                    e instanceof Error
+                        ? {
+                              // prototypes get lost, so turn error into plain object
+                              ...e,
+                              message: e.message,
+                              name: e.name,
+                              stack: e.stack,
+                          }
+                        : e,
             };
         }
     };
