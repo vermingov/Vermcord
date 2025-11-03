709453a8955907b33c307d07588969975cea33aa
Verm <vermin.gov@proton.me>
2025-11-03 18:59:46 +0100

    Selective Friend Remover

--- src/Vencord.ts
+++ src/Vencord.ts
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
 // DO NOT REMOVE UNLESS YOU WISH TO FACE THE WRATH OF THE CIRCULAR DEPENDENCY DEMON!!!!!!!
 import "~plugins";
@@ -57,15 +57,16 @@ async function syncSettings() {
     // pre-check for local shared settings
     if (
         Settings.cloud.authenticated &&
-        !await dsGet("Vencord_cloudSecret") // this has been enabled due to local settings share or some other bug
+        !(await dsGet("Vencord_cloudSecret")) // this has been enabled due to local settings share or some other bug
     ) {
         // show a notification letting them know and tell them how to fix it
         showNotification({
             title: "Cloud Integrations",
-            body: "We've noticed you have cloud integrations enabled in another client! Due to limitations, you will " +
+            body:
+                "We've noticed you have cloud integrations enabled in another client! Due to limitations, you will " +
                 "need to re-authenticate to continue using them. Click here to go to the settings page to do so!",
             color: "var(--yellow-360)",
-            onClick: () => SettingsRouter.open("VencordCloud")
+            onClick: () => SettingsRouter.open("VencordCloud"),
         });
         return;
     }
@@ -77,7 +78,8 @@ async function syncSettings() {
         if (localStorage.Vencord_settingsDirty) {
             await putCloudSettings();
             delete localStorage.Vencord_settingsDirty;
-        } else if (await getCloudSettings(false)) { // if we synchronized something (false means no sync)
+        } else if (await getCloudSettings(false)) {
+            // if we synchronized something (false means no sync)
             // we show a notification here instead of allowing getCloudSettings() to show one to declutter the amount of
             // potential notifications that might occur. getCloudSettings() will always send a notification regardless if
             // there was an error to notify the user, but besides that we only want to show one notification instead of all
@@ -86,7 +88,7 @@ async function syncSettings() {
                 title: "Cloud Settings",
                 body: "Your settings have been updated! Click here to restart to fully apply changes!",
                 color: "var(--green-360)",
-                onClick: relaunch
+                onClick: relaunch,
             });
         }
     }
@@ -99,11 +101,15 @@ async function runUpdateCheck() {
         if (notifiedForUpdatesThisSession) return;
         notifiedForUpdatesThisSession = true;
 
-        setTimeout(() => showNotification({
-            permanent: true,
-            noPersist: true,
-            ...data
-        }), 10_000);
+        setTimeout(
+            () =>
+                showNotification({
+                    permanent: true,
+                    noPersist: true,
+                    ...data,
+                }),
+            10_000,
+        );
     };
 
     try {
@@ -114,9 +120,9 @@ async function runUpdateCheck() {
             await update();
             if (Settings.autoUpdateNotification) {
                 notify({
-                    title: "Vencord has been updated!",
+                    title: "Vermcord has been updated!",
                     body: "Click here to restart",
-                    onClick: relaunch
+                    onClick: relaunch,
                 });
             }
             return;
@@ -125,7 +131,7 @@ async function runUpdateCheck() {
         notify({
             title: "A Vencord update is available!",
             body: "Click here to view the update",
-            onClick: openUpdaterModal!
+            onClick: openUpdaterModal!,
         });
     } catch (err) {
         UpdateLogger.error("Failed to check for updates", err);
@@ -148,7 +154,9 @@ async function init() {
     }
 
     if (IS_DEV) {
-        const pendingPatches = patches.filter(p => !p.all && p.predicate?.() !== false);
+        const pendingPatches = patches.filter(
+            (p) => !p.all && p.predicate?.() !== false,
+        );
         if (pendingPatches.length)
             PMLogger.warn(
                 "Webpack has finished initialising, but some patches haven't been applied yet.",
@@ -156,7 +164,10 @@ async function init() {
                 "that all plugins are working as intended.",
                 "You are seeing this warning because this is a Development build of Vencord.",
                 "\nThe following patches have not been applied:",
-                "\n\n" + pendingPatches.map(p => `${p.plugin}: ${p.find}`).join("\n")
+                "\n\n" +
+                    pendingPatches
+                        .map((p) => `${p.plugin}: ${p.find}`)
+                        .join("\n"),
             );
     }
 }
@@ -164,13 +175,18 @@ async function init() {
 startAllPlugins(StartAt.Init);
 init();
 
-document.addEventListener("DOMContentLoaded", () => {
-    addVencordUiStyles();
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+        addVencordUiStyles();
 
-    startAllPlugins(StartAt.DOMContentLoaded);
+        startAllPlugins(StartAt.DOMContentLoaded);
 
-    // FIXME
-    if (IS_DISCORD_DESKTOP && Settings.winNativeTitleBar && IS_WINDOWS) {
-        createAndAppendStyle("vencord-native-titlebar-style").textContent = "[class*=titleBar]{display: none!important}";
-    }
-}, { once: true });
+        // FIXME
+        if (IS_DISCORD_DESKTOP && Settings.winNativeTitleBar && IS_WINDOWS) {
+            createAndAppendStyle("vencord-native-titlebar-style").textContent =
+                "[class*=titleBar]{display: none!important}";
+        }
+    },
+    { once: true },
+);
